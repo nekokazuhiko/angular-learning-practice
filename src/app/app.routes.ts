1ea94078d19dcb6473cafb02895c9de21a5e3f81
nekoYC <neko@yocue.com>
2024-10-16 18:23:11 +0800

    變更component import方式，轉換成loadComponent節能省電

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,63 +1,67 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { BlogComponent } from './components/blog/blog.component';
-import { FaqComponent } from './components/faq/faq.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { PricingComponent } from './components/pricing/pricing.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { ErrorsComponent } from './components/errors/errors.component';
-import {BlogIndexComponent} from './components/blog/blog-index/blog-index.component';
-import {BlogPostComponent} from "./components/blog/blog-post/blog-post.component";
+// import { HomeComponent } from './components/home/home.component';
+// import { AboutComponent } from './components/about/about.component';
+// import { BlogComponent } from './components/blog/blog.component';
+// import { FaqComponent } from './components/faq/faq.component';
+// import { ContactComponent } from './components/contact/contact.component';
+// import { PricingComponent } from './components/pricing/pricing.component';
+// import { PortfolioComponent } from './components/portfolio/portfolio.component';
+// import { ErrorsComponent } from './components/errors/errors.component';
+// import {BlogIndexComponent} from './components/blog/blog-index/blog-index.component';
+// import {BlogPostComponent} from "./components/blog/blog-post/blog-post.component";
 
 export const routes: Routes = [
 
   {
     title: 'Angular Learning Practice | Home',
     path: 'home',
-    component: HomeComponent,
+    loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent),
   },
   {
     title: 'Angular Learning Practice | about',
     path: 'about',
-    component: AboutComponent,
+    loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent),
   },
   {
     title: 'Angular Learning Practice | blog',
     path: 'blog',
-    component: BlogComponent,
+    loadComponent: () => import('./components/blog/blog.component').then(m => m.BlogComponent),
     children: [
       {
         title: 'Angular Learning Practice | blog',
         path: 'blog-index',
-        component: BlogIndexComponent,
+        loadComponent: () => import('./components/blog/blog-index/blog-index.component').then(m => m.BlogIndexComponent),
       },
       {
         title: 'Angular Learning Practice | blog',
         path: 'blog-post',
-        component: BlogPostComponent,
+        loadComponent: () => import('./components/blog/blog-post/blog-post.component').then(m => m.BlogPostComponent),
       }
     ]
   },
   {
     title: 'Angular Learning Practice | faq',
     path: 'faq',
-    component: FaqComponent,
+    loadComponent: () => import('./components/faq/faq.component').then(m => m.FaqComponent),
+    // component: FaqComponent,
   },
   {
     title: 'Angular Learning Practice | contact',
     path: 'contact',
-    component: ContactComponent,
+    loadComponent: () => import('./components/contact/contact.component').then(m => m.ContactComponent),
+    // component: ContactComponent,
   },
   {
     title: 'Angular Learning Practice | pricing',
     path: 'pricing',
-    component: PricingComponent,
+    loadComponent: () => import('./components/pricing/pricing.component').then(m => m.PricingComponent),
+    // component: PricingComponent,
   },
   {
     title: 'Angular Learning Practice | portfolio',
     path: 'portfolio',
-    component: PortfolioComponent,
+    loadComponent: () => import('./components/portfolio/portfolio.component').then(m => m.PortfolioComponent),
+    // component: PortfolioComponent,
   },
   {
     path: '',
@@ -66,6 +70,7 @@ export const routes: Routes = [
   },
   {
     path: '**',
-    component: ErrorsComponent,
+    loadComponent: () => import('./components/errors/errors.component').then(m => m.ErrorsComponent),
+    // component: ErrorsComponent,
   },
 ];
